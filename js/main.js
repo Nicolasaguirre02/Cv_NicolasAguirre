c90127c1b0279536bfb0aea624cce7f2731bd57e
nicolas aguirre <nico.aguirre02@hotmail.com>
2023-09-10 08:31:05 -0300

    formulario html y css

---
+++ js/main.js
@@ -0,0 +1,113 @@
+/* Formulario */
+const formulario = document.getElementById("formulario");
+
+const nombre = document.getElementById("nombre");
+const mail = document.getElementById("nombre");
+const telefono = document.getElementById("nombre");
+const tema = document.getElementById("nombre");
+const descripcion = document.getElementById("nombre");
+
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const nombre = document.getElementById("nombre").value.trim();
+    const mail = document.getElementById("mail").value.trim();
+    const telefono = document.getElementById("telefono").value.trim();
+    const tema = document.getElementById("tema").value.trim();
+    const descripcion = document.getElementById("descripcion").value.trim();
+
+    let resltNombre = validarNombre(nombre, "nombre", "avisoNombre");
+
+    let resltMail = validarMAil(mail, "mail", "avisoMail");
+
+    let resltTelefono = validarTelefono(telefono, "telefono", "avisoTelefono");
+
+    let resltTema = validarTema(tema, "tema", "avisoTema");
+
+    let resltDescripcion = validarDescripcion(descripcion, "descripcion", "avisoDescripcion");
+
+    if(resltNombre && resltMail && resltTelefono && resltTema && resltDescripcion){
+        fetch("https://formsubmit.co/nicolas.aguirre02@hotmail.com", {
+                method: "POST",
+                body: new FormData(formulario)
+            })
+            .then(function(response) {
+                // Maneja la respuesta del servidor aquí
+                console.log(response);
+                alert("Formulario enviado con éxito.");
+            })
+            .catch(function(error) {
+                console.error(error);
+                alert("Hubo un error al enviar el formulario.");
+            });
+    }
+})
+
+function validarNombre(nombre, esValido, esDisplay){
+    if(nombre === ""){
+        document.getElementById(esValido).classList.add("is-invalid");
+        document.getElementsByClassName(esDisplay)[0].style.display = "block";
+        return false;
+    }else{
+        document.getElementById(esValido).classList.remove("is-invalid");
+        document.getElementById(esValido).classList.add("is-valid");
+        document.getElementsByClassName(esDisplay)[0].style.display = "none";
+        return true
+    } 
+}
+
+function validarMAil(mail, esValido, esDisplay){
+    const verMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (verMail.test(mail)) {
+        document.getElementById(esValido).classList.remove("is-invalid");
+        document.getElementById(esValido).classList.add("is-valid");
+        document.getElementsByClassName(esDisplay)[0].style.display = "none";
+        return true
+    } else {
+        document.getElementById(esValido).classList.add("is-invalid");
+        document.getElementsByClassName(esDisplay)[0].style.display = "block";
+        return false;
+    }
+}
+
+function validarTelefono(telefono, esValido, esDisplay){
+    if(telefono.length > 11 || telefono.length < 5 ){
+        document.getElementById(esValido).classList.add("is-invalid");
+        document.getElementsByClassName(esDisplay)[0].style.display = "block";
+        return false;
+    }else{
+        document.getElementById(esValido).classList.remove("is-invalid");
+        document.getElementById(esValido).classList.add("is-valid");
+        document.getElementsByClassName(esDisplay)[0].style.display = "none";
+        return true
+    } 
+}
+
+function validarTema(tema, esValido, esDisplay){
+    if(tema === ""){
+        document.getElementById(esValido).classList.add("is-invalid");
+        document.getElementsByClassName(esDisplay)[0].style.display = "block";
+        return false;
+    }else{
+        document.getElementById(esValido).classList.remove("is-invalid");
+        document.getElementById(esValido).classList.add("is-valid");
+        document.getElementsByClassName(esDisplay)[0].style.display = "none";
+        return true
+    } 
+}
+
+function validarDescripcion(descripcion, esValido, esDisplay){
+    if(descripcion === ""){
+        document.getElementById(esValido).classList.add("is-invalid");
+        document.getElementsByClassName(esDisplay)[0].style.display = "block";
+        return false;
+    }else{
+        document.getElementById(esValido).classList.remove("is-invalid");
+        document.getElementById(esValido).classList.add("is-valid");
+        document.getElementsByClassName(esDisplay)[0].style.display = "none";
+        return true
+    } 
+}
+
+/* Fin formulario */
